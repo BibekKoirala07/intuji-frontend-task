60ea0c5c32b2cba1703ede3bc9f098c0acef8f71
BibekKoirala07 <www.koiralabibek2058@gmail.com>
2025-03-31 12:53:06 +0545

    index.html completed !!! skeleton

---
+++ utils/overviewData.js
@@ -0,0 +1,32 @@
+const overviewData = [
+  {
+    image: "https://via.placeholder.com/100",
+    heading: "Product 1",
+    description: "This is product 1",
+    price: 100,
+    quantity: 1,
+  },
+  {
+    image: "https://via.placeholder.com/100",
+    heading: "Product 2",
+    description: "This is product 2",
+    price: 200,
+    quantity: 1,
+  },
+  {
+    image: "https://via.placeholder.com/100",
+    heading: "Product 3",
+    description: "This is product 3",
+    price: 150,
+    quantity: 1,
+  },
+  {
+    image: "https://via.placeholder.com/100",
+    heading: "Product 4",
+    description: "This is product 4",
+    price: 250,
+    quantity: 1,
+  },
+];
+
+export default overviewData;
